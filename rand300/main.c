6d47df1d2cf6da1dc95f90d6a1bf6a2e896310ad
zhangzma <88343432@qq.com>
2016-12-09 15:54:53 +0800

    测试c/c++中随机数生成算法

--- rand300/main.c
+++ rand300/main.c
@@ -16,8 +16,39 @@
 
 int main(int argc, char *argv[])
 {
-    srand(time(NULL));
-    RandDat(300);
+    /* srand(time(NULL)); */
+    /* RandDat(300); */
+
+    srand(0x37);
+    char txt[100] = {'\0'};
+    char sn[100] = {'\0'};
+    int  idx = 0;
+    
+    for (int i = 0; i < 10; i++) {
+        txt[idx++] = i + '0';
+    }
+
+    for (int i = 0; i < 26; i++) {
+        txt[idx+i] = i + 'a';
+        txt[idx+i+26] = i + 'A';
+    }
+    idx += 26*2;
+
+    for (int i = 0; i < idx; i++) {
+        int index = rand() % 62;
+        if (txt[index] != '#') {
+            sn[i] = txt[index];
+            txt[index] = '#';
+        } else {
+            i--;
+        }
+    }
+
+    printf("sn = %s\n", sn);
+    printf("src = %s\n", txt);
+    /* printf("sizeof(txt) = %d", idx); */
+
+
     return 0;
 }
 
