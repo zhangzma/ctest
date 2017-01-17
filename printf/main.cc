10b9e52090221ffbf89b203c17b6cd1a2e76ce39
zhangzma <88343432@qq.com>
2017-01-17 15:35:08 +0800

    测试sprintf函数用法。

--- printf/main.cc
+++ printf/main.cc
@@ -2,13 +2,31 @@
 
 #include <iostream>
 #include <cstdio>
+#include <string.h>
 
+void test_sprintf();
 int main(int argc, char *argv[])
 {
-    int ival = 0x0034;
-    int *pi = &ival;
-    printf("ival = 0x%04x, pi = %p\n", ival, pi);
+    // int ival = 0x0034;
+    // int *pi = &ival;
+    // printf("ival = 0x%04x, pi = %p\n", ival, pi);
+
+    test_sprintf();
 
     return 0;
+}
+
+void test_sprintf()
+{
+    char str[80];
+    int  n;
+    char *pstr = str;
+
+    n = sprintf(str, "Fly mode changed: old mode = %d, current mode = %d. changed reaseon: %d",
+                1, 2, 3);
+
+    printf("str = %s\n", pstr);
+    printf("n = %d\n", n);
+    printf("strlen(pstr) = %d\n", strlen(pstr));
 }
 
